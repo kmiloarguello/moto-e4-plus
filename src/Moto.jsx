2a29185f6c8a861fdc5c999d35ca38ec6167eb2c
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2018-07-15 22:22:34 -0500

    Improving layout in middle devices

--- src/Moto.jsx
+++ src/Moto.jsx
@@ -6,25 +6,23 @@ export default class Moto extends Component {
     return (
       <div className={styles.container}>
         <section className={styles.section1}>
-          <div class={styles.A}>
-            A
-          </div>
-          <div class={styles.B} >B</div>
+          <div class={styles.A}>A</div>
+          <div class={styles.B}>B</div>
         </section>
         <section className={styles.section2}>
-          <div class={styles.A} >A</div>
-          <div class={styles.B} >B</div>
-          <div class={styles.C} >C</div>
+          <div class={styles.A}>A</div>
+          <div class={styles.B}>B</div>
+          <div class={styles.C}>C</div>
         </section>
         <section className={styles.section3}>
-          <div class={styles.A} >A</div>
+          <div class={styles.A}>A</div>
         </section>
         <section className={styles.section4}>
-          <div class={styles.A} >A</div>
-          <div class={styles.B} >B</div>
+          <div class={styles.A}>A</div>
+          <div class={styles.B}>B</div>
         </section>
         <section className={styles.section5}>
-          <div class={styles.A} >A</div>
+          <div class={styles.A}>A</div>
         </section>
       </div>
     );
